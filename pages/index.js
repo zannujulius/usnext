6dc5b7b45a5a0ac9a346d4f494e223c59c16a541
zannujulius <zannujulius14@gmail.com>
2022-03-02 17:19:14 +0100

    worked on the home component

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,27 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Navbar from "../components/Navbar";
+import { HiArrowNarrowLeft } from "react-icons/hi";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <div>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat ea rem
-        atque quo facilis esse autem ipsam, explicabo tempore earum!
+    <div className={styles.homecover}>
+      <div className={styles.homenavlink}>
+        <div className={styles.homenavlinkcover}>
+          <HiArrowNarrowLeft
+            size={15}
+            color={"dodgerblue"}
+            className="arrowleft"
+          />
+        </div>
+        <div className={styles.homenavlinktext}>podglad sprawy</div>
+      </div>
+      <div className={styles.homecontent}>
+        <div className={styles.hometopcontent}>
+          <div className={styles.hometopcontenttitle}>
+            Umowa na dosatarczeine cementu
+          </div>
+        </div>
       </div>
     </div>
   );
