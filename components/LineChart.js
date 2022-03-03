eff93b5c8652b2525c6e02e9fb75f60479705739
zannujulius <zannujulius14@gmail.com>
2022-03-03 17:16:11 +0100

    added twoj container

--- components/LineChart.js
+++ components/LineChart.js
@@ -1,99 +1,90 @@
-import React, { Component, useRef, useState } from "react";
+import React, { useRef } from "react";
 import { Area, Line } from "react-chartjs-2";
 import Chart from "chart.js/auto";
 
-class LineChart extends Component {
-  // constructor(props){
-  //   super(props)
-  //   this.state = {
-  //     data:
-  //   }
-  // }
-  // getChartData = (canvas) => {};
-  render() {
-    return (
-      <div className="chart-container">
-        <div className="chart-title">Saldo biezqce sprawy</div>
-        <div className="chart-value">
-          12 5000 <span className="chart-value__title"> PLN</span>
-        </div>
-        <div className="line-chart">
-          <Line
-            data={{
-              labels: [65, 59, 80, 81, 56, 55, 40],
-              datasets: [
-                {
-                  lineTension: 0.1,
-                  borderColor: "transparent",
-                  borderCapStyle: "butt",
-                  borderDash: [],
-                  borderDashOffset: 0.0,
-                  borderJoinStyle: "miter",
-                  pointBorderColor: "rgba(75,192,192,1)",
-                  pointBackgroundColor: "#fff",
-                  pointBorderWidth: 3,
-                  pointHoverRadius: 5,
-                  pointHoverBackgroundColor: "rgba(75,192,192,1)",
-                  pointHoverBorderColor: "rgba(220,220,220,1)",
-                  pointHoverBorderWidth: 2,
-                  pointRadius: 3,
-                  pointHitRadius: 10,
-                  data: [65, 59, 80, 81, 56, 55, 40],
-                },
-              ],
-            }}
-            options={{
-              responsive: true,
-              maintainAspectRatio: false,
-              backgroundColor: "#0183d0",
-              fill: true,
-              label: "",
-              plugins: {
-                title: {
-                  display: false,
-                },
-                legend: {
-                  display: false,
-                },
-                tooltip: {
-                  intersect: true,
-                },
-              },
-              scales: {
-                x: {
-                  grid: {
-                    display: true,
-                    borderDash: [10],
-                    borderDashOffset: 20,
-                    borderWidth: 0,
-                  },
-                  ticks: {
-                    font: {
-                      size: 10,
-                    },
-                  },
-                },
-                y: {
-                  grid: {
-                    display: false,
-                    borderWidth: 0,
-                  },
-                  ticks: {
-                    callback: function (value, index, ticks) {
-                      return value + "K";
-                    },
-                    font: {
-                      size: 10,
-                    },
-                  },
-                },
-              },
-            }}
-          />
-        </div>
+const LineChart = () => {
+  return (
+    <div className="chart-container">
+      <div className="chart-title">Saldo biezqce sprawy</div>
+      <div className="chart-value">
+        12 5000 <span className="chart-value__title"> PLN</span>
       </div>
-    );
-  }
-}
+      <div className="line-chart">
+        <Line
+          data={{
+            labels: [65, 59, 80, 81, 56, 55, 40],
+            datasets: [
+              {
+                lineTension: 0.1,
+                borderColor: "transparent",
+                borderCapStyle: "butt",
+                borderDash: [],
+                borderDashOffset: 0.0,
+                borderJoinStyle: "miter",
+                pointBorderColor: "rgba(75,192,192,1)",
+                pointBackgroundColor: "#fff",
+                pointBorderWidth: 3,
+                pointHoverRadius: 5,
+                pointHoverBackgroundColor: "rgba(75,192,192,1)",
+                pointHoverBorderColor: "rgba(220,220,220,1)",
+                pointHoverBorderWidth: 2,
+                pointRadius: 3,
+                pointHitRadius: 10,
+                data: [65, 59, 80, 81, 56, 55, 40],
+              },
+            ],
+          }}
+          options={{
+            responsive: true,
+            maintainAspectRatio: false,
+            backgroundColor: "#0183d0",
+            fill: true,
+            label: "",
+            plugins: {
+              title: {
+                display: false,
+              },
+              legend: {
+                display: false,
+              },
+              tooltip: {
+                intersect: true,
+              },
+            },
+            scales: {
+              x: {
+                grid: {
+                  display: true,
+                  borderDash: [10],
+                  borderDashOffset: 20,
+                  borderWidth: 0,
+                },
+                ticks: {
+                  font: {
+                    size: 10,
+                  },
+                },
+              },
+              y: {
+                grid: {
+                  display: false,
+                  borderWidth: 0,
+                },
+                ticks: {
+                  callback: function (value, index, ticks) {
+                    return value + "K";
+                  },
+                  font: {
+                    size: 10,
+                  },
+                },
+              },
+            },
+          }}
+        />
+      </div>
+    </div>
+  );
+};
 
 export default LineChart;
--- pages/index.js
+++ pages/index.js
@@ -5,10 +5,42 @@ import Navbar from "../components/Navbar";
 import { HiArrowNarrowLeft } from "react-icons/hi";
 import { Line } from "react-chartjs-2";
 import { BsBookmark, BsFillReplyFill, BsThreeDots } from "react-icons/bs";
+import { MdCheckCircle } from "react-icons/md";
 import LineChart from "../components/LineChart";
 import { RiErrorWarningLine } from "react-icons/ri";
+import { IoClose } from "react-icons/io5";
 
 export default function Home() {
+  const twojData = [
+    {
+      title: "kraj",
+      caption: "Polska",
+    },
+    {
+      title: "nip",
+      caption: "643 967 94 40",
+    },
+    {
+      title: "Nazwa firmy",
+      caption: "Janbudoplex Sp. z o.o",
+    },
+    {
+      title: "region",
+      caption: "53212123",
+    },
+    {
+      title: "KRS",
+      caption: "000012718",
+    },
+    {
+      title: "wojewodztwo",
+      caption: "Mazowieckie",
+    },
+    {
+      title: "Kod pocztowy",
+      caption: "023512718",
+    },
+  ];
   return (
     <div className={styles.homecover}>
       <div className={styles.homenavlink}>
@@ -89,6 +121,59 @@ export default function Home() {
           <div className="chart-filter__item">1M</div>
           <div className="chart-filter__item">1Y</div>
         </div>
+
+        <div className={"hr-divider"}> </div>
+        <div className="tagi-container">
+          <div className="tagi-title">Tagi</div>
+          <div className="tagi-caption__cover">
+            <div className="tagi-caption__text">#Wierzytelnosci</div>
+            <div className="tagi-caption__icon">
+              <div className="tagi-icon">
+                <IoClose
+                  size={16}
+                  style={{
+                    fill: "tomato",
+                  }}
+                />
+              </div>
+              <div className="tagi-icon">
+                <MdCheckCircle
+                  size={16}
+                  style={{
+                    fill: "#0183d0",
+                  }}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={"hr-divider"}> </div>
+        <div className="twoj-container">
+          <div className="towj-title">Twoj Kontrahent</div>
+          <div className="twoj-content__cover">
+            {twojData.map((item, i) => (
+              <div className="twoj-item" key={i}>
+                <div className="twoj-title__text">{item.title}</div>
+                <div className="twoj-content">{item.caption}</div>
+              </div>
+            ))}
+          </div>
+          <div className="bottom-title__cover">
+            <div className="bottom-icon">
+              <MdCheckCircle
+                size={16}
+                style={{
+                  fill: "#15c040",
+                }}
+              />
+            </div>
+            <div className="bottom-title">
+              Wykupiles raport o tymkontranhecie
+            </div>
+            <div className="bottom-tag">Zobacz raport</div>
+          </div>
+        </div>
+        <div className={"hr-divider"}> </div>
       </div>
     </div>
   );
